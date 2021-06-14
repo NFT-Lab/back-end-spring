916cf2401739e77e7dd208ee42a92d69e9a00313
Andrea Cecchin <crashctr92@gmail.com>
2021-06-14 15:25:28 +0200

    add file .env and .ignore

--- NFTService/src/main/java/configuration/WebConfiguration.java
+++ NFTService/src/main/java/configuration/WebConfiguration.java
@@ -12,6 +12,7 @@ import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.tx.gas.StaticGasProvider;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import io.nfteam.nftlab.contracts.NFTLabStore;
 import io.nfteam.nftlab.services.NFTContractService;
 import io.nfteam.nftlab.services.NFTETHContractService;
@@ -27,23 +28,24 @@ public class WebConfiguration {
     }
     @Bean
     public NFTContractService getNFTContractService() throws Exception{
+    	Dotenv dotenv = Dotenv.load();
     	//da qui 
     	
-    	HttpService service = new HttpService("http://127.0.0.1:7545/");
+    	HttpService service = new HttpService(dotenv.get("ETHEREUM_URL"));
     	
     	Web3j web3j = Web3j.build(service);
     	System.out.println("Ho creato web3j");
     	
-    	Credentials cred = Credentials.create("ea0a035dc7c5e70719d2062dd09bf3c93f94f4d4c0837e78a6ce0316a050502e");
+    	Credentials cred = Credentials.create(dotenv.get("ETHEREUM_PRIVATE_KEY"));
     	System.out.println("Ho creato cred");
     	
     	ContractGasProvider cgp = new StaticGasProvider(BigInteger.valueOf(20000000000L),BigInteger.valueOf(6721975L));
     	System.out.println("Ho creato gas");
     	
-    	NFTLabStore store = NFTETHContractService.loadContract("0x5c4533a6a0e0ccb5cb7d81bb238e9f1c592fed7c",web3j, cred, cgp);
+    	NFTLabStore store = NFTETHContractService.loadContract(dotenv.get("ETHEREUM_CONTRACT_ADDRESS"),web3j, cred, cgp);
     	System.out.println("Ho creato store " + store.getContractAddress());
     	//a qui
-    	IPFSService ipfs = new IPFSPinataService("https://api.pinata.cloud/","e46660a6e97946b6c0fc","3ed15d7caa100d83dd46aba6ec5941af8b14bda22d1169a0435c043c02db3189", new RestTemplate());
+    	IPFSService ipfs = new IPFSPinataService(dotenv.get("PINATA_BASE_URL"), dotenv.get("PINATA_API_KEY"), dotenv.get("PINATA_SECRET_KEY"), new RestTemplate());
     	System.out.println("Ho creato ipfs");
     	
     	return new NFTETHContractService(
